d609db283610fbf711ca6955063f1153b043ef45
ToddLiao <toddliao.dev@gmail.com>
2025-02-05 00:43:17 +0800

    Revert ":bug: Fix: fix service-worker cache"
    This reverts commit 7d4ebe6e9a79b37d125c83e54c13caa5533e8186.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,8 @@
 export const prerender = true;
 import type { LayoutLoad } from './$types';
+import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+
+injectSpeedInsights()
 
 export const load: LayoutLoad = async ({ url }) => {
   return {
