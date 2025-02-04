f127bb1c72497f237400244a41978ad766a72811
ToddLiao <toddliao.dev@gmail.com>
2025-02-05 00:44:05 +0800

    :bug: Fix: remove speed insights

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,5 @@
 export const prerender = true;
 import type { LayoutLoad } from './$types';
-import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
-
-injectSpeedInsights()
 
 export const load: LayoutLoad = async ({ url }) => {
   return {
